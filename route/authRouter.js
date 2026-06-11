e4160761e0e45644a70dc57cf01e77762c7390ec
mahirTheCoder <mahirchowdhury518@gmail.com>
2026-06-11 23:14:49 +0600

    --

--- route/authRouter.js
+++ route/authRouter.js
@@ -6,7 +6,7 @@ const route = express.Router()
 route.post('/signUp' , signUp)
 route.post('/verifyOtp' , verifyOtp)
 route.post('/reSendOtp' , reSendOtp)
-route.post('/signIn' , signIn)
+route.post('/signIn' , signIn) 
 
 
 module.exports = route
